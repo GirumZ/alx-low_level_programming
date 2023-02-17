0ffc5bff1204eb9ceb0c93c4b681c49dc074eb4f
GirumZ <girum.zebe@gmail.com>
2023-02-17 10:37:49 -0800

    Sixth commit for #5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * This program prints single digit numbers
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = i ; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
