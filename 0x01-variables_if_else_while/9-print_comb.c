ecf4343ce1f844ff906bc11c02f6fe8ea55c063f
GirumZ <girum.zebe@gmail.com>
2023-02-17 11:42:44 -0800

    Tenth commit for #9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * This program prints the single digits of base 16
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar('0' + i);
+		if (i != 9)
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
