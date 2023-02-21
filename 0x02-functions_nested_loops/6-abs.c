c66f64e2c25711888fddac7b632c4f4d474382ea
GirumZ <girum.zebe@gmail.com>
2023-02-21 13:44:01 -0800

    Amended 7th commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,21 +2,20 @@
 #include "main.h"
 /**
  * _abs - calculates the absolute value of a number
- * @int: any intiger
+ * @n: any intiger
  * Return: Always 0 for success
  */
-int _abs(int)
+int _abs(int n)
 {
-	int n;
 	int result;
 
 	if (n < 0)
 	{
-		result = n * (-1)
+		result = n * (-1);
 	}
 	else
 	{
-		result = n
+		result = n;
 	}
 	return (result);
 }
