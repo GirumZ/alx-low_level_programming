6805e62fe1f701c86ff180aa3d65162caf7d15bd
GirumZ <girum.zebe@gmail.com>
2023-02-23 03:34:09 -0800

    First commit for more functions project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result mult
+ */
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - prints from 1 to nine
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		_putchar(i);
+	}
+}
