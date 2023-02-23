a37b092899368fe9d34eedb31a93764124121bfa
GirumZ <girum.zebe@gmail.com>
2023-02-23 14:30:55 -0800

    6th commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,26 +10,31 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
 	for (i = 0 ; i < n ; i++)
 	{
-		if (n > 0)
+		for (j = 0 ; j < n ; j++)
 		{
-			for (j = 0 ; j < n ; j++)
+			if (j == i)
 			{
-				if (j == i)
-				{
-					_putchar(92);
-				}
-				else if (j > i)
-				{
-					break;
-				}
-				else
-				{
-					_putchar(32);
-				}
+				_putchar(92);
+			}
+			else if (j > i)
+			{
+				continue;
+			}
+			else
+			{
+				_putchar(32);
 			}
 		}
 		_putchar('\n');
 	}
+	}
+
 }
