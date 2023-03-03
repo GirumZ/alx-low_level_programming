99819cb4906dd1bd33b9d72c54ef4b2392cca20e
GirumZ <girum.zebe@gmail.com>
2023-03-03 01:57:15 -0800

    9th commit for more pointers project

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,38 +2,23 @@
 /**
  * leet - encodes a string into 1337
  * @s: the string to be encoded
- * Rerutn: s
+ * Return: pointer to s
  */
 char *leet(char *s)
 {
-	int i;
+	char *r = s;
+	char x[] = {'a', 'e', 'o', 't', 'l'};
+	char y[] = {4, 3, 0, 7, 1};
+	int i = 0;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
+	while (*s)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+		for (i = 0 ; i < 5 ; i++)
 		{
-			s[i] = '4';
+			if (*s == x[i] || *s == x[i] - 32)
+				*s = y[i] + '0';
 		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = '3';
-		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i] = '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = '7';
-		}
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = '1';
-		}
-		else
-		{
-			continue;
-		}
+		s++
 	}
-	return (s);
+	return (r);
 }
