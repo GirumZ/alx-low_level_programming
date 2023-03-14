6100a51c2be54d0630d6673d5cb4bd2de174187e
GirumZ <girum.zebe@gmail.com>
2023-03-14 09:52:52 -0700

    8th commit for malloc and free project

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * count - counts words
+ * @s: string to be counted
+ *
+ * Return: number of words
+ */
+int count(char *s)
+{
+	int i, j, k;
+
+	i = 0;
+	k = 0;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		if (s[j] == ' ')
+			i = 0;
+		else if (i == 0)
+		{
+			i = 1;
+			k++;
+		}
+	}
+
+	return (k);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **array, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	array = (char **) malloc(sizeof(char *) * (words + 1));
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				array[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	array[k] = NULL;
+
+	return (array);
+}
