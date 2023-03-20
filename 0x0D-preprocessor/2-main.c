c7eaa14970d00c84ba46d520cb7d63987ba882f7
GirumZ <girum.zebe@gmail.com>
2023-03-20 10:21:17 -0700

    print the name of the source file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - prints the name of the source file
  * Return: if success 0
@@ -6,6 +7,6 @@
 
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
