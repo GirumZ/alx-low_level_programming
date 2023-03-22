f93c1d37aa098d0658594906f169b580d13e9dac
GirumZ <girum.zebe@gmail.com>
2023-03-22 03:25:43 -0700

    A function to print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints name
+ * @name: the name to be printed
+ * @f: pointer to a callback function)
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This directory is for the function pointer project.
