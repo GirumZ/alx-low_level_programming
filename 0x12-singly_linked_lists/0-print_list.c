d973dedd70a3954a02a5745273610738ba039682
GirumZ <girum.zebe@gmail.com>
2023-03-30 05:35:39 -0700

    After testing #0 task (print_list)

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of list_t
+ * @h: a pointer to the list
+ * Return: number of nodes printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *current;
+
+	if (h == NULL)
+		return (0);
+	current = h;
+	while (h != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			count++;
+		}
+		else
+		{
+			printf("[%u] %s\n", current->len, current->str);
+			count++;
+		}
+		if (current->next == NULL)
+			break;
+		current = current->next;
+	}
+	return (count);
+}
