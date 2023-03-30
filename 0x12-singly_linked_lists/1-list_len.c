85284273b31dbd32e37944c7b3571ace22e1853e
GirumZ <girum.zebe@gmail.com>
2023-03-30 06:12:33 -0700

    After testing #1 task

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - calculates the length of a linked list
+ * @h: pointer to the linked list
+ * Return: length of a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *current;
+
+	curretnt = h;
+	while (h)
+	{
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
