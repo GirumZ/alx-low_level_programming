a1886ba9a13b829383e62557abeaf096e61594a2
GirumZ <girum.zebe@gmail.com>
2023-04-04 07:51:10 -0700

    After 4th fix on #9 task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,26 +1,5 @@
 #include "lists.h"
 /**
- * listint_len - returns the nummber of elements of listint_t list
- * @h: is the head of the list
- * Return: the number of elements of the list
- */
-
-size_t listint_len(const listint_t *h)
-{
-	/* current points to each node at a time*/
-	const listint_t *current = h;
-	/* count - counts the number of elements */
-	size_t count = 0;
-
-	while (current)
-	{
-		current = current->next;
-		count++;
-	}
-	return (count);
-}
-
-/**
  * insert_nodeint_at_index - inserts node at an index
  * @head: head of the list
  * @idx: index where the node is inserted
@@ -28,25 +7,35 @@ size_t listint_len(const listint_t *h)
  *
  * Return: a pointer to the new node
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current, *new;
-	size_t i, list_len;
+	size_t i;
+	listint_t *new, *h;
 
-	if ((*head) == NULL)
+	h = *head;
+	if (idx != 0)
+	{
+		for (i = 0; (i < (idx - 1) && h != NULL) ; i++)
+		{
+			h = h->next;
+		}
+	}
+
+	if ((h == NULL) && (idx != 0))
 		return (NULL);
-	list_len = listint_len(*head);
-	if (idx > list_len)
-		return (NULL);
-	current = (*head);
-	for (i = 0 ; i < (idx - 1) ; i++)
-		current = current->next;
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
-	new->next = current->next;
-	current->next = new;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	new->next = h->next;
+	h->next = new;
 	return (new);
 }
