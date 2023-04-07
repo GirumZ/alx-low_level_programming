0c92c6c3a88a19a593fd9ecd525b720314e9a464
GirumZ <girum.zebe@gmail.com>
2023-04-07 02:54:51 -0700

    After fix on #5 task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,22 @@
 #include "main.h"
 /**
- * _sqrt_recursion - calculates the square root of a number
- * @n: the number whose square root is calculated
- * Return: the square root (if found) if not found, -1
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number
+ * Return: the natural square root of the number if successful
+ * and -1 if failure
  */
-
 int _sqrt_recursion(int n)
 {
-	return (test(n, 1));
-}
+	int i, count = 0;
 
-/**
- * test - tests for the square root between 0 and half of the number
- * @n: the number
- * @i: the number being tested
- *
- * Return: square root of a number(if found) else -1
- */
-
-int test(int n, int i)
-{
-	if (n < 0)
-		return (-1);
-	else if (n == 0)
+	if (n == 0)
 		return (0);
-	else if ((n % i == 0) && (n / i == i))
-		return (i);
-	else if (i <= n / 2)
-		return (test(n, i + 1));
+	for (i = 1 ; n >= i ; i += 2)
+	{
+		n = n - i;
+		count++;
+		if (n == 0)
+			return (count);
+	}
 	return (-1);
 }
