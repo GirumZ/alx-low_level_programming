a27e9709fa6f8f2ec7d626baac0b9f6174d89130
GirumZ <girum.zebe@gmail.com>
2023-04-21 13:46:15 -0700

    print_all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,82 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints output depending of the format specified
+ * @format: the format specified
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	char specifier[4] = {'c', 'i', 'f', 's'};
+	size_t i, j;
+	va_list args;
+	fn_list print_fns[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	va_start(args, format);
+	i = 0;
+	while (i < strlen(format))
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == specifier[j])
+			{
+				print_fns[j].func(args);
+				if (i < (strlen(format) - 1))
+					printf(",");
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+}
+
+
+/**
+ * print_char - prints character
+ * @args: argument to be printed
+ * Return: NULL
+ */
+
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints integer
+ * @args: argument to be printed
+ * Return: void
+ */
+
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+/**
+ * print_float - prints float
+ * @args: argument to be printed
+ * Return: void
+ */
+
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+/**
+ * print_string - prints strings
+ * @args: the argument to be printed
+ * Return: void
+ */
+
+void print_string(va_list args)
+{
+	printf("%s", va_arg(args, char *));
+}
