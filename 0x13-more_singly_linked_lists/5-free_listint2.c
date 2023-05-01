1a3a3b0e7046114e1c8448843d83b6ccb14e6d5a
GirumZ <girum.zebe@gmail.com>
2023-05-01 01:59:35 -0700

    free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a linked list and set the head to NULL
+ * @head: head of the list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *h;
+
+	h = *head;
+	while (h && head)
+	{
+		current = h->next;
+		free(h);
+		h = current;
+	}
+	*head = NULL;
+}
