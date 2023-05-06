c9b5e9282f75d7740e698d5a5b82bd8ee1915652
GirumZ <girum.zebe@gmail.com>
2023-05-06 09:51:44 -0700

    hello_hoberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .data
+	to_print: db "Hello, Holberton", 10
+section .text
+	global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, to_print
+	mov rdx, 18
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
