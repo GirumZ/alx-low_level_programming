06cabf0b9469201c314de1f4cbf2cd071fefb1d9
GirumZ <girum.zebe@gmail.com>
2023-05-08 08:50:18 -0700

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 		true_write = write(fd, text_content, len);
 	close(fd);
-	if (true_write = -1)
+	if (true_write == -1)
 		return (-1);
 	return (1);
 }
