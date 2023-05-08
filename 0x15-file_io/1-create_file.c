17ddd7add13baf216c4a060785b44a96534265ec
GirumZ <girum.zebe@gmail.com>
2023-05-08 06:11:19 -0700

    create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: the name of the file
+ * @text_content: the content to be writen in the new file
+ *
+ * Return: 1 for success and -1 for failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, true_write;
+
+	len = strlen(text_content);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+		true_write = write(fd, text_content, len);
+	close(fd);
+	if (true_write < 0)
+		return (-1);
+	return (1);
+}
