8adec109169dd7e5e45a23408234daa563119dd4
GirumZ <girum.zebe@gmail.com>
2023-06-16 02:01:39 -0700

    [Create]: a function named insert_dnodeint.c that adds node at an index in a list

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts node at an index of a list
+ * @h: head of the list
+ * @n: data element of the node
+ * @idx: the index of the list
+ *
+ * Return: the new node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	size_t i;
+	dlistint_t *new, *current;
+
+	current = *h;
+	if (idx != 0)
+	{
+		for (i = 0 ; (i < (idx - 1) && current != NULL) ; i++)
+			current = current->next;
+	}
+	if ((current == NULL) && (idx != 0))
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	if (idx > 0)
+	{
+		new->next = current->next;
+		new->prev = current;
+		(current->next)->prev = new;
+		current->next = new;
+	}
+	else
+	{
+		new->next = *h;
+		new->prev = NULL;
+		*h = new;
+	}
+	new->n = n;
+	return (new);
+}
