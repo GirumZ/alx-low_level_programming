5dbeabcc82373d5c85c140b6bd6a9e92e8ae5967
GirumZ <girum.zebe@gmail.com>
2023-09-08 17:03:48 -0700

    [Create]: A c file containing hash_table_create function that creates a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the table
+ * Return: a pointer to the hesh table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long i;
+
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+	return (table);
+}
